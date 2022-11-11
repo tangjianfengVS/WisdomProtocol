1d9f02bb2565ecb1631e488bb3586378144eec44
jianfeng.tang <jianfeng.tang@reacheng.com>
2022-11-11 16:30:47 +0800

    init

---
+++ WisdomProtocol/WisdomProtocol/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  WisdomProtocol
+//
+//  Created by 汤建锋 on 2022/11/11.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
