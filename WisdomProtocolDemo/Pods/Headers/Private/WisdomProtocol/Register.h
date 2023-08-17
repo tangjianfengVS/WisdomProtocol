056cf0a929210ce7a3814711639d357ccbfb9df0
jianfeng.tang <jianfeng.tang@reacheng.com>
2023-08-17 11:33:46 +0800

    rem

--- WisdomProtocolDemo/Pods/Headers/Private/WisdomProtocol/Register.h
+++
@@ -1,1 +0,0 @@
-../../../WisdomProtocol/Source/Register.h
--- WisdomProtocolDemo/Pods/Target Support Files/WisdomProtocol-framework/WisdomProtocol-framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WisdomProtocol_framework : NSObject
-@end
-@implementation PodsDummy_WisdomProtocol_framework
-@end
